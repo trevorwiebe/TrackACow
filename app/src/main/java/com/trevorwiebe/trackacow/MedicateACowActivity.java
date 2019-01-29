90205754b7f942a803ccfef4a498d0df513252bd
trevor <thisistrevor4@gmail.com>
2019-01-29 16:34:05 -0600

    fixed bug where you could save a medicated cow with out saving any drugs

--- app/src/main/java/com/trevorwiebe/trackacow/MedicateACowActivity.java
+++ app/src/main/java/com/trevorwiebe/trackacow/MedicateACowActivity.java
@@ -127,6 +127,11 @@ public class MedicateACowActivity extends AppCompatActivity {
                         }
                     }
 
+                    if(drugList.size() == 0){
+                        Snackbar.make(view, "You must select at least one drug before saving", Snackbar.LENGTH_LONG).show();
+                        return;
+                    }
+
                     int tagNumber = Integer.parseInt(mTagName.getText().toString());
                     String notes = mNotes.getText().toString();
 
