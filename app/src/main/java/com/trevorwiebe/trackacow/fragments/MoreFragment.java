4e77f85cadb7cb6636381a841e64ee989525dd01
Trevor <thisistrevor4@gmail.com>
2022-09-19 15:51:02 -0500

    add manage rations to more screen, update more fragment to include compound textViews

--- app/src/main/java/com/trevorwiebe/trackacow/fragments/MoreFragment.java
+++ app/src/main/java/com/trevorwiebe/trackacow/fragments/MoreFragment.java
@@ -1,32 +1,21 @@
 package com.trevorwiebe.trackacow.fragments;
 
-import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-
-import com.google.firebase.auth.FirebaseAuth;
+import android.widget.TextView;
 import com.trevorwiebe.trackacow.R;
 import com.trevorwiebe.trackacow.activities.ArchivesActivity;
 import com.trevorwiebe.trackacow.activities.ManageDrugsActivity;
 import com.trevorwiebe.trackacow.activities.ManagePensActivity;
 import com.trevorwiebe.trackacow.activities.SettingsActivity;
-import com.trevorwiebe.trackacow.dataLoaders.misc.InsertAllLocalChangeToCloud;
-import com.trevorwiebe.trackacow.utils.Utility;
 
-public class MoreFragment extends Fragment implements InsertAllLocalChangeToCloud.OnAllLocalDbInsertedToCloud {
-
-    private static final String TAG = "MoreFragment";
-
-    private AlertDialog.Builder mBackingUpData;
+public class MoreFragment extends Fragment{
 
     @Nullable
     @Override
@@ -34,16 +23,7 @@ public class MoreFragment extends Fragment implements InsertAllLocalChangeToClou
 
         View rootView = inflater.inflate(R.layout.fragment_more, container, false);
 
-        LinearLayout manageDrugs = rootView.findViewById(R.id.manage_drugs_layout);
-        manageDrugs.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent manageDrugsIntent = new Intent(getContext(), ManageDrugsActivity.class);
-                startActivity(manageDrugsIntent);
-            }
-        });
-
-        LinearLayout managePens = rootView.findViewById(R.id.manage_pens_layout);
+        TextView managePens = rootView.findViewById(R.id.manage_pen_tv);
         managePens.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -52,7 +32,24 @@ public class MoreFragment extends Fragment implements InsertAllLocalChangeToClou
             }
         });
 
-        LinearLayout archives = rootView.findViewById(R.id.archive_layout);
+        TextView manageDrugs = rootView.findViewById(R.id.manage_drugs_tv);
+        manageDrugs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent manageDrugsIntent = new Intent(getContext(), ManageDrugsActivity.class);
+                startActivity(manageDrugsIntent);
+            }
+        });
+
+        TextView manageRations = rootView.findViewById(R.id.manage_rations_tv);
+        manageRations.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        TextView archives = rootView.findViewById(R.id.archives_tv);
         archives.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -61,7 +58,7 @@ public class MoreFragment extends Fragment implements InsertAllLocalChangeToClou
             }
         });
 
-        LinearLayout settings = rootView.findViewById(R.id.settings_layout);
+        TextView settings = rootView.findViewById(R.id.more_settings_tv);
         settings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -72,19 +69,4 @@ public class MoreFragment extends Fragment implements InsertAllLocalChangeToClou
 
         return rootView;
     }
-
-
-    @Override
-    public void onAllLocalDbInsertedToCloud(int resultCode) {
-        if (mBackingUpData != null) {
-            AlertDialog createDialog = mBackingUpData.create();
-            createDialog.dismiss();
-        }
-        FirebaseAuth.getInstance().signOut();
-        if (getActivity() != null) {
-            getActivity().finish();
-        }
-        Utility.setNewDataToUpload(getContext(), false);
-    }
-
 }
