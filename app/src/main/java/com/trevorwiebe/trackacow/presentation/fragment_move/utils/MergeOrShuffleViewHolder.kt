f9c51ee91487081a530d542258961b34ba7e262a
Trevor <thisistrevor4@gmail.com>
2022-10-08 16:04:24 -0500

    add merge button, simplify MergeOrShuffleViewHolder.kt

--- app/src/main/java/com/trevorwiebe/trackacow/presentation/fragment_move/utils/MergeOrShuffleViewHolder.kt
+++ app/src/main/java/com/trevorwiebe/trackacow/presentation/fragment_move/utils/MergeOrShuffleViewHolder.kt
@@ -1,16 +1,12 @@
 package com.trevorwiebe.trackacow.presentation.fragment_move.utils
 
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.trevorwiebe.trackacow.R
 
 class MergeOrShuffleViewHolder(view: View): RecyclerView.ViewHolder(view) {
-
-    val title: TextView
-
-    init {
-        title = view.findViewById(R.id.item_shuffle_merge_or_split)
-    }
-
+    val title: TextView = view.findViewById(R.id.item_shuffle_merge_or_split)
+    val mergeOrSplit: Button = view.findViewById(R.id.merge_or_split_btn)
 }
