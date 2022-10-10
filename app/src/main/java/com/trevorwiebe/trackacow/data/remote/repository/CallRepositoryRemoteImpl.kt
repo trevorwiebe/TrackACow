07c051d1f12a05b65f6569a7cd064a9d45dadd05
Trevor <thisistrevor4@gmail.com>
2022-10-10 18:13:35 -0500

    update CallRepositoryRemoteImpl.kt to use cloud database ID as directory key

--- app/src/main/java/com/trevorwiebe/trackacow/data/remote/repository/CallRepositoryRemoteImpl.kt
+++ app/src/main/java/com/trevorwiebe/trackacow/data/remote/repository/CallRepositoryRemoteImpl.kt
@@ -1,5 +1,6 @@
 package com.trevorwiebe.trackacow.data.remote.repository
 
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.trevorwiebe.trackacow.domain.models.call.CallModel
 import com.trevorwiebe.trackacow.domain.repository.remote.CallRepositoryRemote
@@ -10,8 +11,10 @@ class CallRepositoryRemoteImpl(
 ): CallRepositoryRemote {
 
     override suspend fun insertCallRemote(callModel: CallModel) {
-        val firebaseId = callModel.primaryKey
-        firebaseDatabase.getReference("$databasePath/$firebaseId").setValue(callModel)
+        val databaseReference: DatabaseReference = firebaseDatabase.getReference(databasePath)
+        val cloudDatabaseId = databaseReference.push().key
+        callModel.id = cloudDatabaseId ?: ""
+        databaseReference.child(cloudDatabaseId?:"").setValue(callModel)
     }
 
     override suspend fun insertCallListRemote(callModelList: List<CallModel>) {
