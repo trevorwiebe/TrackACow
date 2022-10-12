c128d615ea2dac2e62bec76cb3fa57f444119ff7
Trevor <thisistrevor4@gmail.com>
2022-10-12 11:08:00 -0500

    add code to auto select password or email if one is wrong

--- app/src/main/java/com/trevorwiebe/trackacow/presentation/activities/SignInActivity.java
+++ app/src/main/java/com/trevorwiebe/trackacow/presentation/activities/SignInActivity.java
@@ -12,7 +12,6 @@ import androidx.core.content.ContextCompat;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
@@ -32,8 +31,11 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.GoogleAuthProvider;
 import com.trevorwiebe.trackacow.R;
+
+import java.util.Objects;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -43,6 +45,25 @@ public class SignInActivity extends AppCompatActivity {
     private GoogleSignInClient mGoogleSignInClient;
     private static final int RC_SIGN_IN = 477;
     private static final int CREATE_ACCOUNT_CODE = 848;
+
+    public static final String NO_ERROR = "NO_ERROR";
+    public static final String ERROR_WRONG_PASSWORD = "ERROR_WRONG_PASSWORD";
+    public static final String ERROR_USER_MISMATCH = "ERROR_USER_MISMATCH";
+    public static final String ERROR_INVALID_EMAIL = "ERROR_INVALID_EMAIL";
+    public static final String ERROR_USER_DISABLED = "ERROR_USER_DISABLED";
+    public static final String ERROR_INVALID_CUSTOM_TOKEN = "ERROR_INVALID_CUSTOM_TOKEN";
+    public static final String ERROR_CUSTOM_TOKEN_MISMATCH = "ERROR_CUSTOM_TOKEN_MISMATCH";
+    public static final String ERROR_INVALID_CREDENTIAL = "ERROR_INVALID_CREDENTIAL";
+    public static final String ERROR_REQUIRES_RECENT_LOGIN = "ERROR_REQUIRES_RECENT_LOGIN";
+    public static final String ERROR_ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL = "ERROR_ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL";
+    public static final String ERROR_EMAIL_ALREADY_IN_USE = "ERROR_EMAIL_ALREADY_IN_USE";
+    public static final String ERROR_CREDENTIAL_ALREADY_IN_USE = "ERROR_CREDENTIAL_ALREADY_IN_USE";
+    public static final String ERROR_USER_TOKEN_EXPIRED = "ERROR_USER_TOKEN_EXPIRED";
+    public static final String ERROR_USER_NOT_FOUND = "ERROR_USER_NOT_FOUND";
+    public static final String ERROR_INVALID_USER_TOKEN = "ERROR_INVALID_USER_TOKEN";
+    public static final String ERROR_OPERATION_NOT_ALLOWED = "ERROR_OPERATION_NOT_ALLOWED";
+    public static final String ERROR_WEAK_PASSWORD = "ERROR_WEAK_PASSWORD";
+    public static final String ERROR_MISSING_EMAIL = "ERROR_MISSING_EMAIL";
 
     private EditText mEmail;
     private EditText mPassword;
@@ -77,13 +98,26 @@ public class SignInActivity extends AppCompatActivity {
         mSignInBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                boolean shouldSave = true;
+
+                if (mPassword.length() == 0) {
+                    mPassword.requestFocus();
+                    mPassword.setError("Please enter your password");
+                    shouldSave = false;
+                }else{
+                    mPassword.setError(null);
+                }
+
                 if (mEmail.length() == 0) {
                     mEmail.requestFocus();
                     mEmail.setError("Please enter your email");
-                } else if (mPassword.length() == 0) {
-                    mPassword.requestFocus();
-                    mPassword.setError("Please enter your password");
-                } else {
+                    shouldSave = false;
+                }else{
+                    mEmail.setError(null);
+                }
+
+                if(shouldSave){
 
                     mSigningIn.setVisibility(View.VISIBLE);
 
@@ -101,9 +135,12 @@ public class SignInActivity extends AppCompatActivity {
                                     } else {
                                         mSigningIn.setVisibility(View.INVISIBLE);
 
-                                        String errorMessage = task.getException().getLocalizedMessage();
+                                        String errorCode = ((FirebaseAuthException) task.getException()).getErrorCode();
+                                        String errorMessage = Objects.requireNonNull(task.getException()).getLocalizedMessage();
+                                        String messageTitle = "Error signing in";
 
-                                        showMessage("Error signing in", errorMessage);
+                                        showMessage(messageTitle, errorMessage, errorCode);
+
                                     }
                                 }
                             });
@@ -163,10 +200,10 @@ public class SignInActivity extends AppCompatActivity {
                                                 @Override
                                                 public void onComplete(@NonNull Task<Void> task) {
                                                     if (task.isSuccessful()) {
-                                                        Log.d(TAG, "Email sent.");
-                                                        showMessage("Success!", "The link has been sent to " + emailAddress.getText().toString());
+                                                        showMessage("Success!", "The link has been sent to " + emailAddress.getText().toString(), NO_ERROR);
                                                     } else {
-                                                        showMessage("Failure", task.getException().getLocalizedMessage());
+                                                        String errorCode = ((FirebaseAuthException) task.getException()).getErrorCode();
+                                                        showMessage("Failure", task.getException().getLocalizedMessage(), errorCode);
                                                     }
                                                 }
                                             });
@@ -201,7 +238,8 @@ public class SignInActivity extends AppCompatActivity {
                 fireBaseAuthWithGoogle(account);
             } catch (ApiException e) {
                 String errorMessage = e.getLocalizedMessage();
-                showMessage("Error signing in", errorMessage);
+                String errorCode = ((FirebaseAuthException) task.getException()).getErrorCode();
+                showMessage("Error signing in", errorMessage, errorCode);
             }
         }
 
@@ -222,20 +260,38 @@ public class SignInActivity extends AppCompatActivity {
                         } else {
                             // If sign in fails, display a message to the user.
                             String errorMessage = task.getException().getLocalizedMessage();
-                            showMessage("Error signing in", errorMessage);
+                            String errorCode = ((FirebaseAuthException) task.getException()).getErrorCode();
+                            showMessage("Error signing in", errorMessage, errorCode);
                         }
                     }
                 });
     }
 
-    private void showMessage(String title, String errorMessage) {
+    private void showMessage(String title, String errorMessage, String errorType) {
         AlertDialog.Builder signInError = new AlertDialog.Builder(SignInActivity.this);
         signInError.setTitle(title);
         signInError.setMessage(errorMessage);
         signInError.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-
+                if(errorType == ERROR_INVALID_EMAIL
+                    || errorType == ERROR_USER_DISABLED
+                    || errorType == ERROR_USER_MISMATCH
+                    || errorType == ERROR_ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL
+                    || errorType == ERROR_EMAIL_ALREADY_IN_USE
+                    || errorType == ERROR_CREDENTIAL_ALREADY_IN_USE
+                    || errorType == ERROR_USER_NOT_FOUND
+                    || errorType == ERROR_MISSING_EMAIL
+                ){
+                    mEmail.requestFocus();
+                    mEmail.selectAll();
+                }else if(errorType == ERROR_INVALID_CREDENTIAL
+                    || errorType == ERROR_WRONG_PASSWORD
+                    || errorType == ERROR_WEAK_PASSWORD
+                ){
+                    mPassword.requestFocus();
+                    mPassword.selectAll();
+                }
             }
         });
         signInError.show();
