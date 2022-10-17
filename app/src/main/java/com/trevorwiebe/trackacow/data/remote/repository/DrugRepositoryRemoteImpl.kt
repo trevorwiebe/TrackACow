164488ac4e518f677934d57572dbb6ac16ad5978
Trevor <thisistrevor4@gmail.com>
2022-10-17 16:09:45 -0500

    fix bug where if updating drug or pen will null cloudDatabaseId it will overwrite all other records

--- app/src/main/java/com/trevorwiebe/trackacow/data/remote/repository/DrugRepositoryRemoteImpl.kt
+++ app/src/main/java/com/trevorwiebe/trackacow/data/remote/repository/DrugRepositoryRemoteImpl.kt
@@ -1,6 +1,5 @@
 package com.trevorwiebe.trackacow.data.remote.repository
 
-import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.trevorwiebe.trackacow.domain.models.drug.DrugModel
 import com.trevorwiebe.trackacow.domain.repository.remote.DrugRepositoryRemote
@@ -11,9 +10,11 @@ class DrugRepositoryRemoteImpl(
 ): DrugRepositoryRemote {
 
     override fun insertDrug(drugModel: DrugModel) {
-        firebaseDatabase.getReference(
-            "$databasePath/${drugModel.drugCloudDatabaseId}"
-        ).setValue(drugModel)
+        if(drugModel.drugCloudDatabaseId.isNotEmpty()) {
+            firebaseDatabase.getReference(
+                "$databasePath/${drugModel.drugCloudDatabaseId}"
+            ).setValue(drugModel)
+        }
     }
 
 }
--- app/src/main/java/com/trevorwiebe/trackacow/data/remote/repository/PenRepositoryRemoteImpl.kt
+++ app/src/main/java/com/trevorwiebe/trackacow/data/remote/repository/PenRepositoryRemoteImpl.kt
@@ -1,6 +1,5 @@
 package com.trevorwiebe.trackacow.data.remote.repository
 
-import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.trevorwiebe.trackacow.domain.models.pen.PenModel
 import com.trevorwiebe.trackacow.domain.repository.remote.PenRepositoryRemote
@@ -12,11 +11,20 @@ class PenRepositoryRemoteImpl(
 
     // Use this for insert and update
     override suspend fun insertPenRemote(penModel: PenModel) {
-        val databaseReference: DatabaseReference = firebaseDatabase.getReference(databasePath)
-        databaseReference.child(penModel.penCloudDatabaseId?:"").setValue(penModel)
+        // make sure is not null or empty
+        if(!penModel.penCloudDatabaseId.isNullOrEmpty()) {
+            firebaseDatabase.getReference(
+                "$databasePath/${penModel.penCloudDatabaseId}"
+            ).setValue(penModel)
+        }
     }
 
     override suspend fun deletePenRemote(penModel: PenModel) {
-        firebaseDatabase.getReference("$databasePath/${penModel.penCloudDatabaseId}").removeValue()
+        // make sure is not null or empty
+        if(!penModel.penCloudDatabaseId.isNullOrEmpty()) {
+            firebaseDatabase.getReference(
+                "$databasePath/${penModel.penCloudDatabaseId}"
+            ).removeValue()
+        }
     }
 }
