1711643cb40086a80bd0be5f4ce481c139426e4c
trevor <thisistrevor4@gmail.com>
2023-03-29 17:32:14 -0500

    add parcelize package

--- app/src/main/java/com/trevorwiebe/trackacow/presentation/fragment_pen_feed/ui_model/FeedPenListUiModel.kt
+++ app/src/main/java/com/trevorwiebe/trackacow/presentation/fragment_pen_feed/ui_model/FeedPenListUiModel.kt
@@ -1,10 +1,13 @@
 package com.trevorwiebe.trackacow.presentation.fragment_pen_feed.ui_model
 
+import android.os.Parcelable
 import com.trevorwiebe.trackacow.domain.models.compound_model.CallAndRationModel
 import com.trevorwiebe.trackacow.domain.models.feed.FeedModel
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class FeedPenListUiModel(
     val date: Long,
     val callAndRationModel: CallAndRationModel,
     val feedList: List<FeedModel>
-)
+) : Parcelable
