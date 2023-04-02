fb972b32a708059733937cf86b01c359766201cd
trevor <thisistrevor4@gmail.com>
2023-04-02 18:36:09 -0500

    convert ItemClickListener.java to .kt

--- app/src/main/java/com/trevorwiebe/trackacow/domain/utils/ItemClickListener.kt
+++ app/src/main/java/com/trevorwiebe/trackacow/domain/utils/ItemClickListener.kt
@@ -1,58 +1,49 @@
-package com.trevorwiebe.trackacow.domain.utils;
+package com.trevorwiebe.trackacow.domain.utils
 
-import android.content.Context;
+import android.content.Context
+import android.view.GestureDetector
+import android.view.GestureDetector.SimpleOnGestureListener
+import android.view.MotionEvent
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.OnItemTouchListener
 
-import androidx.recyclerview.widget.RecyclerView;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
-import android.view.View;
-
-public class ItemClickListener implements RecyclerView.OnItemTouchListener{
-
-    private OnItemClickListener mListener;
-
-    public interface OnItemClickListener {
-        void onItemClick(View view, int position);
-
-        void onLongItemClick(View view, int position);
+class ItemClickListener(
+    context: Context?,
+    recyclerView: RecyclerView,
+    private val mListener: OnItemClickListener?
+) : OnItemTouchListener {
+    interface OnItemClickListener {
+        fun onItemClick(view: View, position: Int)
+        fun onLongItemClick(view: View, position: Int)
     }
 
-    GestureDetector mGestureDetector;
+    var mGestureDetector: GestureDetector
 
-    public ItemClickListener(Context context, final RecyclerView recyclerView, OnItemClickListener listener) {
-        mListener = listener;
-        mGestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
-            @Override
-            public boolean onSingleTapUp(MotionEvent e) {
-                return true;
+    init {
+        mGestureDetector = GestureDetector(context, object : SimpleOnGestureListener() {
+            override fun onSingleTapUp(e: MotionEvent): Boolean {
+                return true
             }
 
-            @Override
-            public void onLongPress(MotionEvent e) {
-                View child = recyclerView.findChildViewUnder(e.getX(), e.getY());
+            override fun onLongPress(e: MotionEvent) {
+                val child = recyclerView.findChildViewUnder(e.x, e.y)
                 if (child != null && mListener != null) {
-                    mListener.onLongItemClick(child, recyclerView.getChildAdapterPosition(child));
+                    mListener.onLongItemClick(child, recyclerView.getChildAdapterPosition(child))
                 }
             }
-        });
+        })
     }
 
-    @Override
-    public boolean onInterceptTouchEvent(RecyclerView view, MotionEvent e) {
-        View childView = view.findChildViewUnder(e.getX(), e.getY());
+    override fun onInterceptTouchEvent(view: RecyclerView, e: MotionEvent): Boolean {
+        val childView = view.findChildViewUnder(e.x, e.y)
         if (childView != null && mListener != null && mGestureDetector.onTouchEvent(e)) {
-            mListener.onItemClick(childView, view.getChildAdapterPosition(childView));
-            return true;
+            mListener.onItemClick(childView, view.getChildAdapterPosition(childView))
+            return true
         }
-        return false;
+        return false
     }
 
-    @Override
-    public void onTouchEvent(RecyclerView view, MotionEvent motionEvent) {
-    }
-
-    @Override
-    public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
-    }
-
+    override fun onTouchEvent(view: RecyclerView, motionEvent: MotionEvent) {}
+    override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {}
 }
