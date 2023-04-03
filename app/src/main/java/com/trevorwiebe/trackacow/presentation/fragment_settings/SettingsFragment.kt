9843340e0d67421bb2338f5198f9c5e1c2d53a1c
trevor <thisistrevor4@gmail.com>
2023-04-03 15:24:56 -0500

    convert SettingsFragment.java to .kt

--- app/src/main/java/com/trevorwiebe/trackacow/presentation/fragment_settings/SettingsFragment.kt
+++ app/src/main/java/com/trevorwiebe/trackacow/presentation/fragment_settings/SettingsFragment.kt
@@ -1,169 +1,164 @@
-package com.trevorwiebe.trackacow.presentation.fragment_settings;
+package com.trevorwiebe.trackacow.presentation.fragment_settings
 
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.os.Bundle;
+import android.app.AlertDialog
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.trevorwiebe.trackacow.R
+import com.trevorwiebe.trackacow.domain.dataLoaders.misc.DeleteAllLocalData
+import com.trevorwiebe.trackacow.domain.dataLoaders.misc.InsertAllLocalChangeToCloud
+import com.trevorwiebe.trackacow.domain.dataLoaders.misc.InsertAllLocalChangeToCloud.OnAllLocalDbInsertedToCloud
+import com.trevorwiebe.trackacow.domain.utils.Constants
+import com.trevorwiebe.trackacow.domain.utils.Utility.isThereNewDataToUpload
+import com.trevorwiebe.trackacow.domain.utils.Utility.setLastSync
+import com.trevorwiebe.trackacow.domain.utils.Utility.setNewDataToUpload
 
-import androidx.preference.Preference;
-import androidx.preference.PreferenceFragmentCompat;
+class SettingsFragment : PreferenceFragmentCompat(), OnAllLocalDbInsertedToCloud {
 
-import android.view.LayoutInflater;
-import android.view.View;
+    private lateinit var mContext: Context
+    private var mBackingUpData: AlertDialog.Builder? = null
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.trevorwiebe.trackacow.R;
-import com.trevorwiebe.trackacow.domain.dataLoaders.misc.DeleteAllLocalData;
-import com.trevorwiebe.trackacow.domain.dataLoaders.misc.InsertAllLocalChangeToCloud;
-import com.trevorwiebe.trackacow.domain.utils.Constants;
-import com.trevorwiebe.trackacow.domain.utils.Utility;
+    override fun onCreatePreferences(bundle: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.settings, rootKey)
 
-public class SettingsFragment extends PreferenceFragmentCompat implements InsertAllLocalChangeToCloud.OnAllLocalDbInsertedToCloud {
+        val accountNamePref = findPreference<Preference>("account_name")
+        val user = FirebaseAuth.getInstance().currentUser
+        if (user != null) {
+            val name = user.displayName
+            val email = user.email
+            val accountName = "$name, $email"
+            if (accountNamePref != null) {
+                accountNamePref.summary = accountName
+            }
+        }
 
-    private Context mContext;
+        val signOutPref = findPreference<Preference>("sign_out")
+        if (signOutPref != null) {
+            signOutPref.onPreferenceClickListener = Preference.OnPreferenceClickListener {
+                if (isThereNewDataToUpload(mContext)) {
+                    val thereIsDataDialog: AlertDialog.Builder = AlertDialog.Builder(
+                        context
+                    )
+                    thereIsDataDialog.setPositiveButton(
+                        "Backup and sign out"
+                    ) { _, _ ->
+                        setNewDataToUpload(requireContext(), false)
+                        mBackingUpData = AlertDialog.Builder(context)
+                        val dialogLoadingLayout: View = LayoutInflater.from(context)
+                            .inflate(R.layout.dialog_inserting_data_to_cloud, null)
+                        mBackingUpData!!.setView(dialogLoadingLayout)
+                        mBackingUpData!!.show()
+                        InsertAllLocalChangeToCloud(this@SettingsFragment).execute(
+                            context
+                        )
+                    }
+                    thereIsDataDialog.setNeutralButton(
+                        "Sign out anyway"
+                    ) { _, _ -> signOut() }
+                    thereIsDataDialog.setTitle("Information will be lost")
+                    thereIsDataDialog.setMessage("There is information that hasn't been saved to the cloud.  Signing out will delete this data.  Backup to cloud to save you data. You must have an internet connection prior to backing up.")
+                    thereIsDataDialog.show()
+                } else {
+                    signOut()
+                }
+                false
+            }
+        }
 
-    private AlertDialog.Builder mBackingUpData;
+        val fiveStarReview = findPreference<Preference>("five_star_review")
+        if (fiveStarReview != null) {
+            fiveStarReview.onPreferenceClickListener = Preference.OnPreferenceClickListener {
+                val packageName = "com.trevorwiebe.trackacow"
+                try {
+                    startActivity(
+                        Intent(
+                            Intent.ACTION_VIEW,
+                            Uri.parse("market://details?id=$packageName")
+                        )
+                    )
+                } catch (e: ActivityNotFoundException) {
+                    startActivity(
+                        Intent(
+                            Intent.ACTION_VIEW,
+                            Uri.parse("https://play.google.com/store/apps/details?id=$packageName")
+                        )
+                    )
+                }
+                false
+            }
+        }
 
-    private static final String TAG = "SettingsFragment";
-
-    @Override
-    public void onCreatePreferences(Bundle bundle, String rootKey) {
-        setPreferencesFromResource(R.xml.settings, rootKey);
-
-        Preference versionPref = findPreference("version");
+        val versionPref = findPreference<Preference>("version")
         try {
             if (versionPref != null) {
-                versionPref.setSummary(appVersion());
+                versionPref.summary = appVersion()
             }
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        Preference accountNamePref = findPreference("account_name");
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if (user != null) {
-            String name = user.getDisplayName();
-            String email = user.getEmail();
-            String accountName = name + ", " + email;
-            if (accountNamePref != null) {
-                accountNamePref.setSummary(accountName);
-            }
-        }
-
-        final Preference offlineMode = findPreference("offline_mode");
-        if (offlineMode != null) {
-            offlineMode.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-                @Override
-                public boolean onPreferenceChange(Preference preference, Object newValue) {
-                    return false;
-                }
-            });
-        }
-
-        Preference signOutPref = findPreference("sign_out");
-        if (signOutPref != null) {
-            signOutPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-                @Override
-                public boolean onPreferenceClick(Preference preference) {
-                    boolean isThereLocalData = Utility.isThereNewDataToUpload(mContext);
-                    if (isThereLocalData) {
-                        AlertDialog.Builder thereIsDataDialog = new AlertDialog.Builder(getContext());
-                        thereIsDataDialog.setPositiveButton("Backup and sign out", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                Utility.setNewDataToUpload(getContext(), false);
-                                mBackingUpData = new AlertDialog.Builder(getContext());
-                                View dialogLoadingLayout = LayoutInflater.from(getContext()).inflate(R.layout.dialog_inserting_data_to_cloud, null);
-                                mBackingUpData.setView(dialogLoadingLayout);
-                                mBackingUpData.show();
-                                new InsertAllLocalChangeToCloud(SettingsFragment.this).execute(getContext());
-                            }
-                        });
-                        thereIsDataDialog.setNeutralButton("Sign out anyway", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                signOut();
-                            }
-                        });
-                        thereIsDataDialog.setTitle("Information will be lost");
-                        thereIsDataDialog.setMessage("There is information that hasn't been saved to the cloud.  Signing out will delete this data.  Backup to cloud to save you data. You must have an internet connection prior to backing up.");
-                        thereIsDataDialog.show();
-                    } else {
-                        signOut();
-                    }
-                    return false;
-                }
-            });
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
         }
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        mContext = context;
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mContext = context
     }
 
-    private String appVersion() throws PackageManager.NameNotFoundException {
-        PackageInfo pInfo = mContext.getPackageManager().getPackageInfo(mContext.getPackageName(), 0);
-        return pInfo.versionName;
+    @Throws(PackageManager.NameNotFoundException::class)
+    private fun appVersion(): String {
+        val pInfo = mContext.packageManager.getPackageInfo(
+            mContext.packageName, 0
+        )
+        return pInfo.versionName
     }
 
-    @Override
-    public void onAllLocalDbInsertedToCloud(int resultCode) {
+    override fun onAllLocalDbInsertedToCloud(resultCode: Int) {
         if (resultCode == Constants.SUCCESS) {
             if (mBackingUpData != null) {
-                AlertDialog createDialog = mBackingUpData.create();
-                createDialog.dismiss();
+                val createDialog = mBackingUpData!!.create()
+                createDialog.dismiss()
             }
-            signOut();
+            signOut()
         } else {
-            String message = "";
-            switch (resultCode) {
-                case Constants.NO_NETWORK_CONNECTION:
-                    message = "You do not have a connection, please connect so you can push your data to the cloud.";
-                    break;
-                case Constants.ERROR_PUSHING_DATA_TO_CLOUD:
-                    message = "There was an error pushing your data to the cloud.";
-                    break;
-                default:
-                    message = "An unknown error occurred while pushing your data to the cloud.";
-                    break;
+            val message: String = when (resultCode) {
+                Constants.NO_NETWORK_CONNECTION -> "You do not have a connection, please connect so you can push your data to the cloud."
+                Constants.ERROR_PUSHING_DATA_TO_CLOUD -> "There was an error pushing your data to the cloud."
+                else -> "An unknown error occurred while pushing your data to the cloud."
             }
-            AlertDialog.Builder errorBackingUpDialog = new AlertDialog.Builder(mContext);
-            errorBackingUpDialog.setTitle("There was an error backing up your data.");
-            errorBackingUpDialog.setMessage(message);
-            errorBackingUpDialog.setPositiveButton("Try again", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    new InsertAllLocalChangeToCloud(SettingsFragment.this).execute(getContext());
-                }
-            });
-            errorBackingUpDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-
-                }
-            });
-            errorBackingUpDialog.setNeutralButton("Sign out anyway", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    signOut();
-                }
-            });
-            errorBackingUpDialog.show();
+            val errorBackingUpDialog = AlertDialog.Builder(mContext)
+            errorBackingUpDialog.setTitle("There was an error backing up your data.")
+            errorBackingUpDialog.setMessage(message)
+            errorBackingUpDialog.setPositiveButton(
+                "Try again"
+            ) { _, _ ->
+                InsertAllLocalChangeToCloud(this@SettingsFragment).execute(
+                    context
+                )
+            }
+            errorBackingUpDialog.setNegativeButton(
+                "Cancel"
+            ) { _, _ -> }
+            errorBackingUpDialog.setNeutralButton(
+                "Sign out anyway"
+            ) { _, _ -> signOut() }
+            errorBackingUpDialog.show()
         }
     }
 
-    private void signOut() {
-        FirebaseAuth.getInstance().signOut();
-        if (getActivity() != null) {
-            getActivity().finish();
+    private fun signOut() {
+        FirebaseAuth.getInstance().signOut()
+        if (activity != null) {
+            requireActivity().finish()
         }
-        new DeleteAllLocalData().execute(getContext());
-        Utility.setNewDataToUpload(mContext, false);
-        Utility.setLastSync(mContext, 0);
+        DeleteAllLocalData().execute(context)
+        setNewDataToUpload((mContext), false)
+        setLastSync((mContext), 0)
     }
 }
