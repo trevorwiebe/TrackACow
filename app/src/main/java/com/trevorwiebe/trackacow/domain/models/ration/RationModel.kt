7dd195b37b221530ed984bc8943cc58ecb33bc42
trevor <thisistrevor4@gmail.com>
2023-04-12 10:33:31 -0500

    update parcelize import in RationModel.kt

--- app/src/main/java/com/trevorwiebe/trackacow/domain/models/ration/RationModel.kt
+++ app/src/main/java/com/trevorwiebe/trackacow/domain/models/ration/RationModel.kt
@@ -1,7 +1,7 @@
 package com.trevorwiebe.trackacow.domain.models.ration
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class RationModel(
