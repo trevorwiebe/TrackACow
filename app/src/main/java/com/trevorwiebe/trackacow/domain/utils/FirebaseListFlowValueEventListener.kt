2f2f7424a214b5b4e400fa9e58307bde1e16e489
trevor <thisistrevor4@gmail.com>
2023-04-28 13:44:54 -0500

    add extension function to handle Query

--- app/src/main/java/com/trevorwiebe/trackacow/domain/utils/FirebaseListFlowValueEventListener.kt
+++ app/src/main/java/com/trevorwiebe/trackacow/domain/utils/FirebaseListFlowValueEventListener.kt
@@ -3,6 +3,7 @@ package com.trevorwiebe.trackacow.domain.utils
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.Query
 import com.google.firebase.database.ValueEventListener
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.channels.awaitClose
@@ -10,6 +11,25 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
 
 fun <T> DatabaseReference.addListValueEventListenerFlow(dataType: Class<T>): Flow<List<T>> =
+    callbackFlow {
+        val listener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val list = mutableListOf<T>()
+                dataSnapshot.children.forEach {
+                    it.getValue(dataType)?.let { it1 -> list.add(it1) }
+                }
+                trySend(list)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                cancel(error.message)
+            }
+        }
+        addValueEventListener(listener)
+        awaitClose { removeEventListener(listener) }
+    }
+
+fun <T> Query.addQueryListValueEventListenerFlow(dataType: Class<T>): Flow<List<T>> =
     callbackFlow {
         val listener = object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
