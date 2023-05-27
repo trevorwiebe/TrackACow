1ca70888a7721aed5bbdc5ea54c1df0cd190018f
trevor <thisistrevor4@gmail.com>
2023-05-27 09:55:40 -0500

    add AddEditPensActivity

---
+++ app/src/main/java/com/trevorwiebe/trackacow/presentation/add_edit_pen/AddEditPensActivity.kt
@@ -0,0 +1,12 @@
+package com.trevorwiebe.trackacow.presentation.add_edit_pen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.trevorwiebe.trackacow.R
+
+class AddEditPensActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_edit_pens)
+    }
+}
