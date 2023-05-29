fc049341d038a38e436a5135d7f10585bf2c7c06
trevor <thisistrevor4@gmail.com>
2023-05-28 21:19:39 -0500

    update to use flatMapLatest

--- app/src/main/java/com/trevorwiebe/trackacow/domain/use_cases/drug_use_cases/ReadDrugsUC.kt
+++ app/src/main/java/com/trevorwiebe/trackacow/domain/use_cases/drug_use_cases/ReadDrugsUC.kt
@@ -4,9 +4,9 @@ import com.google.firebase.database.FirebaseDatabase
 import com.trevorwiebe.trackacow.domain.models.drug.DrugModel
 import com.trevorwiebe.trackacow.domain.repository.local.DrugRepository
 import com.trevorwiebe.trackacow.domain.utils.addListValueEventListenerFlow
-import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flatMapConcat
+import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.onStart
 
 data class ReadDrugsUC(
@@ -14,7 +14,7 @@ data class ReadDrugsUC(
     private val firebaseDatabase: FirebaseDatabase,
     private val databaseString: String
 ){
-    @OptIn(FlowPreview::class)
+    @OptIn(ExperimentalCoroutinesApi::class)
     operator fun invoke(): Flow<List<DrugModel>> {
 
         val localFlow = drugRepository.getDrugList()
@@ -22,7 +22,7 @@ data class ReadDrugsUC(
         val cloudFlow = drugDatabaseRef.addListValueEventListenerFlow(DrugModel::class.java)
 
         return cloudFlow
-            .flatMapConcat { cloudData ->
+            .flatMapLatest { cloudData ->
                 drugRepository.insertOrUpdateDrugList(cloudData)
                 localFlow.onStart { emit(cloudData) }
             }
