484a9cbbe7f72845ab939cdc60c210fb7a46cd08
trevor <thisistrevor4@gmail.com>
2023-08-23 10:01:40 -0500

    remove horizontal ration list

--- app/src/main/java/com/trevorwiebe/trackacow/presentation/feed_reports/FeedReportsActivity.kt
+++ app/src/main/java/com/trevorwiebe/trackacow/presentation/feed_reports/FeedReportsActivity.kt
@@ -59,8 +59,6 @@ class FeedReportsActivity : AppCompatActivity() {
         )
     }
 
-    // TODO: remove ration horizontal scroll
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_feed_reports)
