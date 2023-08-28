6c03c17d19349563894821ee9df91d58cda526c3
trevor <thisistrevor4@gmail.com>
2023-08-28 16:29:05 -0500

    add code to keep place when back pressed

--- app/src/main/java/com/trevorwiebe/trackacow/presentation/fragment_feed/FeedContainerFragment.kt
+++ app/src/main/java/com/trevorwiebe/trackacow/presentation/fragment_feed/FeedContainerFragment.kt
@@ -20,6 +20,7 @@ import com.trevorwiebe.trackacow.R
 import com.google.android.material.tabs.TabLayout
 import com.trevorwiebe.trackacow.domain.adapters.FeedPenViewPagerAdapter
 import com.trevorwiebe.trackacow.domain.models.compound_model.PenAndLotModel
+import com.trevorwiebe.trackacow.domain.utils.Utility
 import com.trevorwiebe.trackacow.presentation.manage_pens.ManagePensActivity
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
@@ -28,32 +29,42 @@ import kotlinx.coroutines.launch
 class FeedContainerFragment : Fragment() {
 
     private lateinit var feedPenViewPager: ViewPager
+    private lateinit var mTabs: TabLayout
+    private lateinit var mNoPensLayout: LinearLayout
     private var feedPenViewPagerAdapter: FeedPenViewPagerAdapter? = null
     private lateinit var mContext: Context
     private val feedContainerViewModel: FeedContainerViewModel by viewModels()
     private var mPenAndLotList: List<PenAndLotModel> = emptyList()
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View {
         val view = inflater.inflate(R.layout.fragment_feed, container, false)
         feedPenViewPager = view.findViewById(R.id.feed_pen_view_pager)
-        val tabs = view.findViewById<TabLayout>(R.id.feed_pen_tab_layout)
-        tabs.setupWithViewPager(feedPenViewPager)
-        val noPensLayout = view.findViewById<LinearLayout>(R.id.no_pens_layout)
+        mTabs = view.findViewById(R.id.feed_pen_tab_layout)
+        mTabs.setupWithViewPager(feedPenViewPager)
+        mNoPensLayout = view.findViewById(R.id.no_pens_layout)
         view.findViewById<Button>(R.id.fragment_feed_add_pen_btn)
-            .setOnClickListener {
-                val addPenIntent = Intent(mContext, ManagePensActivity::class.java)
-                startActivity(addPenIntent)
-            }
+                .setOnClickListener {
+                    val addPenIntent = Intent(mContext, ManagePensActivity::class.java)
+                    startActivity(addPenIntent)
+                }
 
         feedPenViewPagerAdapter = FeedPenViewPagerAdapter(childFragmentManager, emptyList())
 
+        feedPenViewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {}
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
+            override fun onPageSelected(position: Int) {
+                Utility.saveLastFeedPen(mContext, position)
+            }
+        })
+
         feedPenViewPager.adapter = feedPenViewPagerAdapter
 
-        lifecycleScope.launch{
+        lifecycleScope.launch {
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
                 feedContainerViewModel.uiState.collect {
 
@@ -61,17 +72,20 @@ class FeedContainerFragment : Fragment() {
 
                     if (mPenAndLotList.isEmpty()) {
                         Handler(Looper.getMainLooper()).postDelayed(
-                            {
-                                if (mPenAndLotList.isEmpty()) {
-                                    noPensLayout.visibility = View.VISIBLE
-                                    tabs.visibility = View.GONE
-                                }
-                            }, 100 // value in milliseconds
+                                {
+                                    if (mPenAndLotList.isEmpty()) {
+                                        mNoPensLayout.visibility = View.VISIBLE
+                                        mTabs.visibility = View.GONE
+                                    }
+                                }, 100 // value in milliseconds
                         )
                     } else {
                         feedPenViewPagerAdapter?.updatePenList(mPenAndLotList)
-                        noPensLayout.visibility = View.GONE
-                        tabs.visibility = View.VISIBLE
+                        Handler(Looper.getMainLooper()).postDelayed({
+                            feedPenViewPager.setCurrentItem(Utility.getLastFeedPen(mContext), false)
+                        }, 10)
+                        mNoPensLayout.visibility = View.GONE
+                        mTabs.visibility = View.VISIBLE
                     }
                 }
             }
