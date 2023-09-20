61ccc9d354adcfd6ce164f5e6c9b19eb2cb81b40
trevor <thisistrevor4@gmail.com>
2023-09-20 11:44:32 -0500

    add TestFirebaseModule, update TrackACowAndroidTest to use Firebase emulator in tests

--- app/src/androidTest/java/com/trevorwiebe/trackacow/test_utils/TrackACowAndroidTest.kt
+++ app/src/androidTest/java/com/trevorwiebe/trackacow/test_utils/TrackACowAndroidTest.kt
@@ -2,11 +2,15 @@ package com.trevorwiebe.trackacow.test_utils
 
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import com.trevorwiebe.trackacow.data.local.AppDatabase
 import dagger.hilt.android.testing.HiltAndroidRule
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 abstract class TrackACowAndroidTest {
@@ -16,17 +20,39 @@ abstract class TrackACowAndroidTest {
 
     protected lateinit var context: Context
 
+    private lateinit var authSignal: CountDownLatch
+
     @Inject
     lateinit var db: AppDatabase
 
+    @Inject
+    lateinit var firebaseDatabase: FirebaseDatabase
+
+    @Inject
+    lateinit var firebaseAuth: FirebaseAuth
+
     @Before
     open fun setUp() {
+        authSignal = CountDownLatch(1)
         context = ApplicationProvider.getApplicationContext()
         hiltRule.inject()
+        if (firebaseAuth.currentUser == null) {
+            firebaseAuth.signInWithEmailAndPassword("test@example.com", "testing")
+                .addOnCompleteListener {
+                    authSignal.countDown()
+                }
+        } else {
+            authSignal.countDown()
+        }
+        authSignal.await(10, TimeUnit.SECONDS)
     }
 
     @After
     open fun tearDown() {
         db.clearAllTables()
+        firebaseDatabase.getReference("/").removeValue()
+        if (firebaseAuth.currentUser != null) {
+            firebaseAuth.signOut()
+        }
     }
 }
---
+++ app/src/androidTest/java/com/trevorwiebe/trackacow/test_utils/di/TestFirebaseModule.kt
@@ -0,0 +1,185 @@
+package com.trevorwiebe.trackacow.test_utils.di
+
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.functions.FirebaseFunctions
+import com.google.firebase.ktx.Firebase
+import com.trevorwiebe.trackacow.data.di.FirebaseModule
+import com.trevorwiebe.trackacow.data.remote.repository.*
+import com.trevorwiebe.trackacow.domain.repository.remote.*
+import com.trevorwiebe.trackacow.domain.use_cases.GetCloudDatabaseId
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.components.SingletonComponent
+import dagger.hilt.testing.TestInstallIn
+import javax.inject.Provider
+import javax.inject.Singleton
+
+@Module
+@TestInstallIn(
+    components = [SingletonComponent::class],
+    replaces = [FirebaseModule::class]
+)
+object TestFirebaseModule {
+
+    @Provides
+    @Singleton
+    fun provideRemoteDatabase(): FirebaseDatabase {
+        val firebaseDatabase = Firebase.database
+        try {
+            firebaseDatabase.useEmulator("10.0.2.2", 9000)
+        } catch (e: IllegalStateException) {
+            Log.e("TAG", "provideRemoteDatabase: IllegalStateException", e)
+        }
+        return firebaseDatabase
+    }
+
+    @Provides
+    @Singleton
+    fun provideFirebaseFunction(): FirebaseFunctions {
+        val firebaseFunctions = FirebaseFunctions.getInstance()
+        try {
+            firebaseFunctions.useEmulator("10.0.2.2", 5001)
+        } catch (e: IllegalStateException) {
+            Log.e("TAG", "provideFirebaseFunction: IllegalStateException", e)
+        }
+        return firebaseFunctions
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideFirebaseAuth(): FirebaseAuth {
+        val firebaseAuth = FirebaseAuth.getInstance()
+        try {
+            firebaseAuth.useEmulator("10.0.2.2", 9099)
+        } catch (e: IllegalStateException) {
+            Log.e("TAG", "provideFirebaseAuth: IllegalStateException", e)
+        }
+        return firebaseAuth
+    }
+
+    @Provides
+    fun provideUserId(auth: FirebaseAuth): String {
+        return auth.currentUser?.uid ?: ""
+    }
+
+    @Provides
+    @Singleton
+    fun provideCloudDatabaseId(
+        databaseReference: FirebaseDatabase,
+        firebaseUserId: Provider<String>
+    ): GetCloudDatabaseId {
+        return GetCloudDatabaseId(databaseReference, firebaseUserId)
+    }
+
+    @Provides
+    @Singleton
+    fun provideRationRepositoryRemote(
+        remoteDb: FirebaseDatabase,
+        firebaseUserId: Provider<String>
+    ): RationRepositoryRemote {
+        return RationRepositoryRemoteImpl(
+            firebaseDatabase = remoteDb,
+            firebaseUserId = firebaseUserId
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun provideCallRepositoryRemote(
+        remoteDb: FirebaseDatabase,
+        firebaseUserId: Provider<String>
+    ): CallRepositoryRemote {
+        return CallRepositoryRemoteImpl(
+            firebaseDatabase = remoteDb,
+            firebaseUserId = firebaseUserId
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun provideCowRemoteRepository(
+        remoteDb: FirebaseDatabase,
+        firebaseUserId: Provider<String>
+    ): CowRepositoryRemote {
+        return CowRepositoryRemoteImpl(
+            firebaseDatabase = remoteDb,
+            firebaseUserId = firebaseUserId
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun provideDrugRepositoryRemote(
+        remoteDb: FirebaseDatabase,
+        firebaseUserId: Provider<String>
+    ): DrugRepositoryRemote {
+        return DrugRepositoryRemoteImpl(
+            firebaseDatabase = remoteDb,
+            firebaseUserId = firebaseUserId
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun provideDrugsGivenRepositoryRemote(
+        remoteDb: FirebaseDatabase,
+        firebaseUserId: Provider<String>
+    ): DrugsGivenRepositoryRemote {
+        return DrugsGivenRepositoryRemoteImpl(
+            firebaseDatabase = remoteDb,
+            firebaseUserId = firebaseUserId
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun providePenRepositoryRemote(
+        remoteDb: FirebaseDatabase,
+        firebaseUserId: Provider<String>
+    ): PenRepositoryRemote {
+        return PenRepositoryRemoteImpl(
+            firebaseDatabase = remoteDb,
+            firebaseUserId = firebaseUserId
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun providesLoadRemoteRepository(
+        remoteDb: FirebaseDatabase,
+        firebaseUserId: Provider<String>
+    ): LoadRemoteRepository {
+        return LoadRemoteRepositoryImpl(
+            firebaseDatabase = remoteDb,
+            firebaseUserId = firebaseUserId
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun provideLotRepositoryRemote(
+        remoteDb: FirebaseDatabase,
+        firebaseUserId: Provider<String>
+    ): LotRepositoryRemote {
+        return LotRepositoryRemoteImpl(
+            firebaseDatabase = remoteDb,
+            firebaseUserId = firebaseUserId
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun provideFeedRepositoryRemote(
+        remoteDb: FirebaseDatabase,
+        firebaseUserId: Provider<String>
+    ): FeedRepositoryRemote {
+        return FeedRepositoryRemoteImpl(
+            firebaseDatabase = remoteDb,
+            firebaseUserId = firebaseUserId
+        )
+    }
+}
