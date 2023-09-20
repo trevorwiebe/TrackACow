f3103544b08751cef5b2e1b1d6da0b3618ba9b30
trevor <thisistrevor4@gmail.com>
2023-09-20 11:45:31 -0500

    add callModel test data generator

---
+++ app/src/test/java/com/trevorwiebe/trackacow/domain/models/CallModel.kt
@@ -0,0 +1,15 @@
+package com.trevorwiebe.trackacow.domain.models
+
+import com.trevorwiebe.trackacow.domain.models.call.CallModel
+import java.util.UUID
+
+fun callModel(): CallModel {
+    return CallModel(
+        callPrimaryKey = 0,
+        callAmount = 50,
+        date = System.currentTimeMillis(),
+        lotId = UUID.randomUUID().toString(),
+        callRationId = null,
+        callCloudDatabaseId = UUID.randomUUID().toString()
+    )
+}
